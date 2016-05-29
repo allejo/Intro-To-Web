acb87a9d8fe04084c20570583b02c5c36d095935
allejo <allejo@me.com>
2016-05-28 18:03:30 -0700

    Introduce color settings + improve theme engine

--- js/footer.js
+++ js/footer.js
@@ -1,1 +1,30 @@
+// Add anchors to headings
 anchors.add().remove('h1');
+
+// Site settings functionality
+function makeRemoveClassHandler(regex) {
+    return function (index, classes) {
+        return classes.split(/\s+/).filter(function (el) {return regex.test(el);}).join(' ');
+    }
+}
+
+$(function () {
+    var colorScheme = readCookie("colorscheme");
+
+    if (colorScheme) {
+        $("html").removeClass(makeRemoveClassHandler(/^t-/));
+        $("html").addClass("t-" + colorScheme);
+    }
+
+    $("#color-scheme input[value=" + colorScheme + "]").prop('checked', true);
+});
+
+$("#color-scheme").change(function () {
+    var $this = $(this);
+    var color = $this.find("[name=ui-colorscheme]:checked").val();
+
+    $("html").removeClass(makeRemoveClassHandler(/^t-/));
+    $("html").addClass("t-" + color);
+
+    createCookie("colorscheme", color);
+});
--- js/head.js
+++ js/head.js
@@ -1,3 +1,34 @@
+// Cookie functionality courtesy of the BZiON team
+// https://git.io/vrHm6
+
+function createCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name,"",-1);
+}
+
+
+// iframe functionality
+
 function setIframeContents (id, content) {
     var iframe = document.getElementById(id);
     iframe = iframe.contentWindow ?
